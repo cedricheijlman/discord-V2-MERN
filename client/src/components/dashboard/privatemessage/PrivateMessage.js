5f9c731be1b6d7b8fb613c0756ca5f816ba57b9b
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-03-03 13:38:40 +0100

    private message frontend

--- client/src/components/dashboard/privatemessage/PrivateMessage.js
+++ client/src/components/dashboard/privatemessage/PrivateMessage.js
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 export const PrivateMessage = () => {
   const [messageInput, setMessageInput] = React.useState("");
   const [messages, setMessages] = React.useState([]);
+  const [friendInfo, setFriendInfo] = React.useState({});
 
   const handleSendMessage = async () => {
     console.log(messageInput);
@@ -18,17 +19,21 @@ export const PrivateMessage = () => {
     }
   };
   let { id } = useParams();
-  console.log(id);
+
   useEffect(() => {
-    //Axios.post("http://localhost:3001/privateMessage", {
-    //  accessKey: localStorage.getItem("accessKey"),
-    // friend: params.
-    // });
+    Axios.post("http://localhost:3001/privateMessage", {
+      accessKey: localStorage.getItem("accessKey"),
+      friend: id,
+    }).then((res) => {
+      console.log(res.data);
+      setFriendInfo(res.data);
+      setMessages(res.data.messages);
+    });
   }, []);
   return (
     <div id="privateMessage">
       <div id="privateMessage__header">
-        <div className="privateMessage__headerLeft">Username</div>
+        <div className="privateMessage__headerLeft">{friendInfo.username}</div>
         <div className="privateMessage__headerRight">
           <input placeholder="Search Message" />
         </div>
