1c518c1f5643da2e6fbc02aea1ccfe8c6afe4fbd
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-03-08 14:32:36 +0100

    populate

--- server/controllers/serverControllers.js
+++ server/controllers/serverControllers.js
@@ -10,6 +10,9 @@ const openServer = async (req, res) => {
     const serverInfo = await Server.findOne({
       _id: serverId,
       "members.userId": decodedJwt.id,
+    }).populate({
+      path: "members.userId",
+      select: "username",
     });
 
     res.status(200).json({ message: "Open Server", serverInfo });
