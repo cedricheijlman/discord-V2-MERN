71c74650c473a9c7ad8ac034a4c5fdf1f9266544
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-03-09 10:49:29 +0100

    server info

--- client/src/components/dashboard/servers/ServerInfo.js
+++ client/src/components/dashboard/servers/ServerInfo.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./serverinfo.css";
 
 const ServerInfo = ({ serverInfo, setServerInfo }) => {
+  const [textChannel, setTextChannel] = useState("General");
   return (
     <div id="serverInfo">
       <div className="serverInfo__serverName">
@@ -10,8 +11,22 @@ const ServerInfo = ({ serverInfo, setServerInfo }) => {
       <div className="serverInfo__textChannelsContainer">
         <p>Text Channels</p>
         <div className="serverInfo__textChannels">
-          <div className="serverInfo__textChannel">General Chat</div>
-          <div className="serverInfo__textChannel">Admin Chat</div>
+          <div
+            onClick={() => setTextChannel("General")}
+            className={`serverInfo__textChannel ${
+              textChannel == "General" ? "check" : ""
+            }`}
+          >
+            General Chat
+          </div>
+          <div
+            onClick={() => setTextChannel("Super")}
+            className={`serverInfo__textChannel ${
+              textChannel == "Super" ? "check" : ""
+            }`}
+          >
+            Super Chat
+          </div>
         </div>
       </div>
     </div>
